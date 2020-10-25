4ae948ef4360fa3a4ebeef285552585ffdd6d30b
Crowdbotics <team@crowdbotics.com>
2020-10-25 17:31:10 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/toca_life_world_21950/MainActivity.java
@@ -0,0 +1,15 @@
+package com.toca_life_world_21950;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "toca_life_world_21950";
+    }
+}
